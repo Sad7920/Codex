e9a3c5d4873b90d259429776c39f1009d690f655
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2023-01-12 18:59:30 +0000

    backend url update

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //Fetch Data from the Server
-  const response = await fetch('http://localhost:4000', {
+  const response = await fetch('https://codex-rqdu.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
